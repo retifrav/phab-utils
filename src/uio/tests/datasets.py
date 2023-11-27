4f6133af8635e513aac071ac5a7e1e6c9d5a06b7
retif <retif@decovar.dev>
2023-11-27 15:41:15 +0100

    Function for deduplicating Pandas table

--- src/uio/tests/datasets.py
+++ src/uio/tests/datasets.py
@@ -21,3 +21,30 @@ def test_merge_pandas() -> None:
 
     assert isinstance(tbl, pandas.DataFrame)
     assert len(tbl) == 9
+
+
+def test_deduplicate_table() -> None:
+    tbl = pandas.DataFrame(
+        [
+            [1, 2, 3],
+            [4, 5, 6],
+            [1, 2, 3],
+            [7, 8, 9],
+            [4, 5, 6],
+            [3, 1, 2],
+            [4, 5, 6]
+        ],
+        index=[1, 2, 3, 4, 5, 6, 7],
+        columns=["a", "b", "c"]
+    )
+
+    deduplicatedTable = pnd.deduplicateTable(tbl, True)
+    assert isinstance(deduplicatedTable, pandas.DataFrame)
+    assert len(deduplicatedTable) == 4
+    assert deduplicatedTable.at[1, "a"] == 1
+    assert deduplicatedTable.at[6, "c"] == 2
+
+    duplicateRows = pnd.deduplicateTable(tbl, False)
+    assert isinstance(duplicateRows, pandas.DataFrame)
+    assert len(duplicateRows) == 3
+    assert duplicateRows.at[7, "b"] == 5
--- src/uio/utility/datasets/pandas.py
+++ src/uio/utility/datasets/pandas.py
@@ -49,3 +49,70 @@ def mergeTables(frames: List[pandas.DataFrame]) -> pandas.DataFrame:
     # logger.debug(mergedTable.head(15))
 
     return mergedTable
+
+
+def deduplicateTable(
+    tbl: pandas.DataFrame,
+    returnUniques: bool = True
+) -> pandas.DataFrame:
+    """
+    Find duplicate rows in the table:
+
+    - with `returnUniques` set to `True`, returns a table without duplicate
+    rows. The first occurrence is always considered to be unique
+    in the resulting table, even if it has duplicates;
+    - with `returnUniques` set to `False` returns only duplicate rows.
+
+    Example:
+
+    ``` py
+    import pandas
+    from uio.utility.datasets import pandas as pnd
+
+    tbl = pandas.DataFrame(
+        [
+            [1, 2, 3],
+            [4, 5, 6],
+            [1, 2, 3],
+            [7, 8, 9],
+            [4, 5, 6],
+            [3, 1, 2],
+            [4, 5, 6]
+        ],
+        index=[1, 2, 3, 4, 5, 6, 7],
+        columns=["a", "b", "c"]
+    )
+
+    deduplicatedTable = pnd.deduplicateTable(tbl, True)
+    # print(deduplicatedTable)
+    #    a  b  c
+    # 1  1  2  3
+    # 2  4  5  6
+    # 4  7  8  9
+    # 6  3  1  2
+    # print(deduplicatedTable.index.values)
+    # [1 2 4 6]
+
+    duplicateRows = pnd.deduplicateTable(tbl, False)
+    # print(duplicateRows)
+    #    a  b  c
+    # 3  1  2  3
+    # 5  4  5  6
+    # 7  4  5  6
+    # print(duplicateRows.index.values)
+    # [3 5 7]
+    ```
+    """
+    duplicateRows = tbl.duplicated()
+    duplicateRows = duplicateRows[duplicateRows].index
+
+    logger.debug(f"Unique rows count: {len(tbl) - len(duplicateRows)}")
+    logger.debug(f"Duplicate rows count: {len(duplicateRows)}")
+    # logger.debug(f"Indexes of duplicate rows: {duplicateRows}")
+
+    duplicatesIndex = tbl.index.isin(duplicateRows)
+
+    return (
+        tbl[~duplicatesIndex] if returnUniques
+        else tbl[duplicatesIndex]
+    )
